787dc543194705c60acb56ccfb7b51d5dd4bcc43
AmirhosseinRB <AmirhosseinRB@users.noreply.github.com>
2025-05-07 14:08:31 +0330

    feat(http): implement setters for HttpDownloaderSetupBuilder

--- src/http/setup.rs
+++ src/http/setup.rs
@@ -1,6 +1,25 @@
-pub struct HttpDownloaderSetupBuilder {}
+use reqwest::Client;
+
+pub struct HttpDownloaderSetupBuilder {
+    client: Option<Client>,
+    raw_url: Option<String>,
+}
+
 impl HttpDownloaderSetupBuilder {
     pub(super) fn default() -> Self {
-        Self {}
+        Self {
+            client: None,
+            raw_url: None,
+        }
+    }
+
+    pub fn client(mut self, client: Client) -> Self {
+        self.client = Some(client);
+        self
+    }
+
+    pub fn url(mut self, raw_url: &str) -> Self {
+        self.raw_url = Some(raw_url.to_string());
+        self
     }
 }
