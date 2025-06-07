d724f57c34dcfb42c58e7aecd213eac2396e0685
AmirhosseinRB <AmirhosseinRB@users.noreply.github.com>
2025-06-07 20:54:30 +0330

    docs(http): add basic crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! # ByteFetch
+//!
+//! `ByteFetch`  is a Rust library that makes HTTP file downloads easier to implement.
+//!
+//! 🚧  **This project is under active development.**
+//!
+//! The current version is an early release to reserve the crate name on [crates.io](https://crates.io/crates/bytefetch).
 mod http;
 pub use http::{HttpDownloader, HttpDownloaderSetupErrors};
 mod manager;
