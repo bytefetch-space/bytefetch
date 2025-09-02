557745ccddcce69032000736b86360b386458829
AmirhosseinRB <AmirhosseinRB@users.noreply.github.com>
2025-09-02 23:07:50 +0330

    refactor(http): extract common options to trait and add delegate methods

--- src/http/options.rs
+++ src/http/options.rs
@@ -4,7 +4,7 @@ use tokio_util::sync::CancellationToken;
 
 use crate::http::{from_state::HttpDownloaderFromStateBuilder, setup::HttpDownloaderSetupBuilder};
 
-pub(super) struct DownloadOptions {
+pub(crate) struct DownloadOptions {
     pub(super) timeout: Option<Duration>,
     pub(super) directory: Option<PathBuf>,
     pub(super) token: CancellationToken,
@@ -22,31 +22,53 @@ impl DownloadOptions {
     }
 }
 
-macro_rules! impl_common_options {
+pub(crate) trait CommonDownloadOptions: Sized {
+    fn options_mut(&mut self) -> &mut DownloadOptions;
+
+    fn timeout(mut self, timeout: Duration) -> Self {
+        self.options_mut().timeout = Some(timeout);
+        self
+    }
+    fn cancel_token(mut self, token: CancellationToken) -> Self {
+        self.options_mut().token = token;
+        self
+    }
+
+    fn directory(mut self, path: PathBuf) -> Self {
+        self.options_mut().directory = Some(path);
+        self
+    }
+
+    fn speed_limit(mut self, kilobytes_per_second: u64) -> Self {
+        self.options_mut().throttle_speed = Some(1024 * kilobytes_per_second);
+        self
+    }
+}
+
+macro_rules! impl_download_options {
     ($t:ty) => {
+        macro_rules! delegate {
+            ($fn_name:ident, $arg_ty:ty) => {
+                pub fn $fn_name(self, value: $arg_ty) -> Self {
+                    <$t as CommonDownloadOptions>::$fn_name(self, value)
+                }
+            };
+        }
+
         impl $t {
-            pub fn timeout(mut self, timeout: Duration) -> Self {
-                self.options.timeout = Some(timeout);
-                self
-            }
+            delegate!(timeout, Duration);
+            delegate!(cancel_token, CancellationToken);
+            delegate!(directory, PathBuf);
+            delegate!(speed_limit, u64);
+        }
 
-            pub fn cancel_token(mut self, token: CancellationToken) -> Self {
-                self.options.token = token;
-                self
-            }
-
-            pub fn directory(mut self, path: PathBuf) -> Self {
-                self.options.directory = Some(path);
-                self
-            }
-
-            pub fn speed_limit(mut self, kilobytes_per_second: u64) -> Self {
-                self.options.throttle_speed = Some(1024 * kilobytes_per_second);
-                self
+        impl CommonDownloadOptions for $t {
+            fn options_mut(&mut self) -> &mut DownloadOptions {
+                &mut self.options
             }
         }
     };
 }
 
-impl_common_options!(HttpDownloaderSetupBuilder);
-impl_common_options!(HttpDownloaderFromStateBuilder);
+impl_download_options!(HttpDownloaderSetupBuilder);
+impl_download_options!(HttpDownloaderFromStateBuilder);
